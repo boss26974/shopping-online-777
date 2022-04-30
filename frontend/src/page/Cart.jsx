6b563358e5d07a620138b26745aae352d2cfec70
boss26974 <pitchaya232@gmail.com>
2022-04-30 19:45:54 +0700

    หน้า cart

--- frontend/src/page/Cart.jsx
+++ frontend/src/page/Cart.jsx
@@ -1,11 +1,52 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
+import "../assets/Cart.css"
 const Cart = () => {
+    const [cart, setCart] = useState(JSON.parse(localStorage.getItem("cart")))
+    const [sumCost, setSumCost] = useState(0)
+
+    useEffect(() => {
+        if(cart != null){
+        let sum = 0
+        cart.map((x) => {
+            sum = sum + (x.Quantity*x.ProductPrice)
+        })
+        setSumCost(sum)
+        }
+    }, [cart])
+
+    const decreaseQuantity = (productId) => {
+        let newCart = cart.map((x) => {
+            if(x.ProductID == productId && x.Quantity == 1){
+            x.Quantity = 1
+            }
+            else if(x.ProductID == productId && x.Quantity > 1){
+            x.Quantity--
+            }
+            return x
+        })
+        setCart(newCart)
+    }
+
+    const increaseQuantity = (productId) => {
+        let newCart = cart.map((x) => {
+        let quantityNow = x.Quantity +  1
+        if(x.ProductID == productId && quantityNow <= x.ProductStock){
+            x.Quantity++
+        }
+        return x
+        })
+        setCart(newCart)
+    }
     return (
         <div>
-            <p>
-                Cart
-            </p>
+            <div className="CartWrapper">
+                <h1 className="CartTitle">YOUR PRODUCT CART</h1>
+                <div className="CartTop">
+                    <button className="CartTopButton">CONTINUE SHOPPING</button>
+                    <span className="CartTopText">Shopping Bag({cart.length})</span>
+                    <button className="CartTopButton">CHECKOUT NOW</button>
+                </div>
+            </div>
         </div>
     );
 }
--- frontend/src/page/Shop.jsx
+++ frontend/src/page/Shop.jsx
@@ -1,13 +1,155 @@
-import React from "react";
+import React, { useState } from "react";
+import "../assets/Shop.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusCircle, faMinusCircle } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 const Shop = () => {
+  const [quantity, setQuantity] = useState(0)
+  const [selector, setSelector] = useState("")
+  const [product, setProduct] = useState({
+    ProductID: 3,
+    ProductSKU: "",
+    ProductName: "Phone",
+    ProductPrice: 2000.0,
+    ProductWeight: 59.0,
+    ProductCartDesc: "Cart",
+    ProductShortDesc: "Short",
+    ProductLongDesc: "LONGLONGLONGLONGLONG",
+    ProductThumb: "",
+    ProductImage: "https://static.zajo.net/content/mediagallery/zajo_dcat/image/product/types/X/9088.png",
+    ProductCategoryID: 2,
+    ProductUpdateDate: "",
+    ProductStock: 20,
+    ProductLive: 1,
+    ProductUnlimited: 1,
+    ProductLocation: "there"
+  })
+
+  const navigate = useNavigate()
+
+  const addToCart = () => {
+    let path = "/home"
+    let newCart = []
+    let cart = JSON.parse(localStorage.getItem("cart"))
+    product.Quantity = quantity
+    if(quantity == 0){
+        return
+    }
+    else if(cart == null){
+      newCart.push(product)
+      alert("new cart")
+      localStorage.setItem("cart", JSON.stringify(newCart))
+      navigate(path)
+      return
+    }
+    let check = cart.filter(x => x.ProductID != product.ProductID)
+    if(check.length < cart.length){
+      newCart = cart.map(x => x)
+      newCart = cart.map((x) => {
+        let plus = x.ProductStock
+        if(x.ProductID == product.ProductID){
+          x.Quantity = x.Quantity + product.Quantity
+        }
+        return x
+      })
+      alert("plus cart")
+    }
+    else{
+      newCart = cart.map(x => x)
+      newCart.push(product)
+      alert("add to cart")
+    }
+    localStorage.setItem("cart", JSON.stringify(newCart))
+    navigate(path)
+  }
+
+  const decreaseQuantity = () => {
+    if(quantity == 0){
+        setQuantity(0)
+    }
+    else if(quantity > 0){
+        setQuantity(Number(quantity) - 1)
+    }
+  }
+
+  const increaseQuantity = () => {
+    let quantityNow = 0
+    if(JSON.parse(localStorage.getItem("cart")) == null){
+        quantityNow = quantity
+    }
+    else{
+        quantityNow = JSON.parse(localStorage.getItem("cart")).filter((x) => {
+            return x.ProductID == product.ProductID
+        })[0].Quantity + quantity
+    }
+
+    if(Number(quantityNow) == Number(product.ProductStock)){
+        setQuantity(quantity)
+    }
+    else if(quantityNow < product.ProductStock){
+        setQuantity(Number(quantity) + 1)
+    }
+  }
+
+  const checkStock = () => {
+    if(product.ProductStock == 0){
+      return "red"
+    }
+    else{
+      return "green"
+    }
+  }
+
     return (
         <div>
-            <p>
-                Shop
-            </p>
+            <div className="Wrapper">
+                <div className="ImgContainer">
+                    <img className="Image" src={product.ProductImage}></img>
+                </div>
+                <div className="InfoContainer">
+                    <h1 className="Title">{product.ProductName}</h1>
+                    <p className="Desc" style={{color: checkStock()}}>
+                        สินค้าคงเหลือ {product.ProductStock} ชิ้น
+                    </p>
+                    <p className="Desc">
+                        {product.ProductLongDesc}
+                    </p>
+                    <span className="Price">$ {product.ProductPrice}</span>
+                    <div className="FilterContainer">
+                        <div className="Filter">
+                            <span className="FilterTitle">Color</span>
+                            <div className="FilterColor" style={{backgroundColor: "black"}}></div>
+                            <div className="FilterColor" style={{backgroundColor: "darkblue"}}></div>
+                            <div className="FilterColor" style={{backgroundColor: "gray"}}></div>
+                        </div>
+                        <div className="Filter">
+                            <span className="FilterTitle">Size</span>
+                            <select className="FilterSize">
+                                <option>s</option>
+                                <option>m</option>
+                                <option>L</option>
+                                <option>xl</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="AddContainer">
+                        <div className="AmountContainer">
+                            <FontAwesomeIcon onClick={decreaseQuantity} icon={faMinusCircle} />
+                            <span className="Amount">{quantity}</span>
+                            <FontAwesomeIcon onClick={increaseQuantity} icon={faPlusCircle} />
+                        </div>
+                        <button className="Button" onClick={addToCart}>ADD TO CART</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
+
+{/* <p>
+<FontAwesomeIcon onClick={() => {alert("plus")}} icon={faPlusCircle} />
+<FontAwesomeIcon onClick={() => {alert("nminus")}} icon={faMinusCircle} />
+</p> */}
 
 export default Shop;
